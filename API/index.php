c5c8e7b4e4b2db1fce08c3d50fadc17ab3e092b8
Devrim Caner ÖKDEM <DevrimCanerOkdem@outlook.com>
2023-09-15 03:03:28 +0300

    Basics
    Basics of the project complate

---
+++ API/index.php
@@ -0,0 +1,106 @@
+
+<?php
+header("Access-Control-Allow-Origin: *");
+$dbHost = 'localhost';
+$dbName = 'todo';
+$dbUser = 'root';
+$dbPass = 'root';
+try{
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName;", $dbUser, $dbPass);
+}
+catch(PDOExeception $e){
+    die($e->getMessage());
+}
+
+$action = $_POST['action'];
+
+switch($action){
+    // List ToDos
+    case 'todos':
+        $query = $db->query('SELECT * FROM todos')->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($query);
+    break;
+    // Add New ToDos
+    case 'add-todo':
+        $todo = $_POST['todo'];
+        $data = [
+            'todo' => $todo,
+            'type' => 1,
+            'done' => 0,
+        ];
+
+        $query = $db->prepare("INSERT INTO todos SET 
+        todo = :todo,
+        type = :type,
+        done = :done");
+        $insert = $query->execute($data);
+        if($insert){
+            $data['id'] = $db->lastInsertId();
+            echo json_encode($data);
+        }
+        else{
+            $data['error'] = 'Error in inserting data';
+        }
+    break;
+    // Change Done ToDos
+    case 'done-todo':
+        $id = $_POST['id'];
+        $done = $_POST['done'];
+        $data = [];
+        if(!$id){
+            $data['error'] = 'Id missing';
+            echo json_encode($data);
+            return;
+        }
+        if(!is_numeric($id)){
+            $data['error'] = 'Id invalid';
+            echo json_encode($data);
+            return;
+        }
+
+        
+        $query = $db->prepare('UPDATE todos SET done = :done WHERE id = :id');
+        $update = $query->execute([
+            'id' => $id,
+            'done' => $done
+        ]);
+        if($update){
+            $query = $db->query('SELECT * FROM todos')->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($query);
+            return;
+        }
+        else{
+            $data['error'] = 'Error in updating data' . $done;
+            echo json_encode($data);
+            return;
+        }
+    break;
+    // Delete ToDos
+    case 'delete-todo':
+        $id = $_POST['id'];
+        $data = [];
+        if(!$id){
+            $data['error'] = 'Id missing';
+            echo json_encode($data);
+            return;
+        }
+        if(!is_numeric($id)){
+            $data['error'] = 'Id invalid';
+            echo json_encode($data);
+            return;
+        }
+
+        $delete = $db->exec('DELETE FROM todos WHERE id = "'. $id . '"');
+        if($delete){
+            $data['deleted'] = 'success';
+            echo json_encode($data);
+            return;
+        }
+        else{
+            $data['error'] = 'Error in deleting data';
+            echo json_encode($data);
+            return;
+        }
+    break;
+}
+?>
--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,24 +1,121 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+
+const [todos, setTodos] = useState();
+const [todo, setTodo] = useState();
+
+  useEffect(() =>{
+    const formData = new FormData();
+    formData.append('action', 'todos');
+    fetch(`${process.env.REACT_APP_ENDPOINT}`,{
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => setTodos(data));
+  },[]);
+
+  const addTodo = () => {
+    if(!todo){
+      alert('Please fill the todo input');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('todo', todo);
+    formData.append('action', 'add-todo');
+    fetch(`${process.env.REACT_APP_ENDPOINT}`,{
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.error){
+        alert(data.error);
+      }
+      else{
+        setTodos([data, ...todos]);
+        setTodo('');
+      }
+    });
+  };
+
+  const deleteTodo = todoId => {
+    if(!todoId){
+      alert('Id missing');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('id', todoId);
+    formData.append('action', 'delete-todo');
+    fetch(`${process.env.REACT_APP_ENDPOINT}`,{
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.error){
+        alert(data.error);
+      }
+      else{
+        setTodos(todos.filter(todo => todo.id != todoId ));
+      }
+    });
+  };
+
+  const doneTodo = (todoId, todoDone) => {
+    console.log(todoDone)
+    todoDone = todoDone === 1 ? 0 : 1;
+    console.log(todoDone)
+    const formData = new FormData();
+    formData.append('id', todoId);
+    formData.append('done', todoDone);
+    formData.append('action', 'done-todo');
+    fetch(`${process.env.REACT_APP_ENDPOINT}`,{
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.error){
+        alert(data.error);
+      }
+      else{
+        setTodos(data);
+      }
+    });
+  };
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ToDo App</h1>
+
+      <div>
+        <input type='text' value={todo || ''} onChange={(e) => setTodo(e.target.value)} placeholder='ToDo'></input>
+        <button onClick={addTodo}>ADD</button>
+      </div>
+
+      {todos && (
+        <ul className='todos'>
+          {todos.map(todo => (
+            <li key={todo.id} className={todo.done == 1 ? 'done' : ''}>
+              <span onClick={() => doneTodo(todo.id, todo.done)}>
+                {todo.todo}
+              </span>
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
